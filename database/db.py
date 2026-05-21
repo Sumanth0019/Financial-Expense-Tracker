d50b90611042ce3980ee8ac5bbbe861ad8fe48ae
sumanth reddy <164669767+Sumanth0019@users.noreply.github.com>
2026-05-22 02:48:27 +0530

    Refactor db.py to simplify database connection logic
    Removed unnecessary code for DATABASE_URL retrieval and simplified the database connection setup.

--- database/db.py
+++ database/db.py
@@ -1,65 +1,34 @@
 import os
-import streamlit as st
+
+from dotenv import load_dotenv
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 
 
 load_dotenv()
 
 
-DATABASE_URL = (
-
-    st.secrets.get(
+DATABASE_URL =  st.secrets.get(
 
         "DATABASE_URL"
 
     )
-
-    or
-
-    os.getenv(
-
-        "DATABASE_URL"
-
-    )
-
-)
 
 
 print(
-
-    "DB:",
-
     DATABASE_URL
-
-)
+)   # add this
 
 
 engine = create_engine(
-
     DATABASE_URL,
-
-    connect_args={
-
-        "sslmode":
-
-        "require"
-
-    },
-
     pool_pre_ping=True
-
 )
 
 
 SessionLocal = sessionmaker(
-
     autocommit=False,
-
     autoflush=False,
-
     bind=engine
-
 )
